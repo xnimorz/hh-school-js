5a466f278faa37e143a672533a2b870353329fd6
Nikita Mostovoy <nik.mostovoy@gmail.com>
2019-12-19 12:45:37 +0300

    slider

---
+++ js/slider.js
@@ -0,0 +1,58 @@
+let active = document.querySelector(".js-slide.slider-content_active");
+
+const showSlide = slideId => {
+  const slide = document.querySelector(`.js-slide[data-slide-id="${slideId}"]`);
+
+  if (slide.classList.contains("slider-content_active")) {
+    return;
+  }
+
+  slide.classList.add("slider-content_show");
+  active.classList.add("slider-content_hide");
+  active.classList.remove("slider-content_active");
+  const previous = active;
+  active = slide;
+
+  document
+    .querySelector(`.js-slide-button[data-slide-id="${slideId}"]`)
+    .classList.add("slider-content-block-dots__dot-container_choosen");
+
+  document
+    .querySelector(
+      `.js-slide-button[data-slide-id="${previous.dataset.slideId}"]`
+    )
+    .classList.remove("slider-content-block-dots__dot-container_choosen");
+
+  active.addEventListener(
+    "animationend",
+    () => {
+      active.classList.add("slider-content_active");
+      active.classList.remove("slider-content_show");
+      previous.classList.remove("slider-content_hide");
+      active.parentNode.insertBefore(active, previous);
+    },
+    { once: true }
+  );
+};
+
+document.querySelector(".js-slide-buttons").addEventListener("click", e => {
+  const data = e.target.dataset;
+  if (!data.slideId) {
+    return;
+  }
+  showSlide(data.slideId);
+});
+
+function nextSlide() {
+  setTimeout(() => {
+    const id = active.dataset.slideId;
+    let next = parseInt(id, 10) + 1;
+    if (next > 2) {
+      next = 1;
+    }
+    showSlide(next);
+    nextSlide();
+  }, 5000);
+}
+
+nextSlide();
